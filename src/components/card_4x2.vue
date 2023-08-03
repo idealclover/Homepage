8d614e36bb5d75a511adf54909ce4d309e24676b
idealclover <idealclover@163.com>
2023-08-04 00:23:14 +0800

    :art: add glass effect

--- src/components/card_4x2.vue
+++ src/components/card_4x2.vue
@@ -4,7 +4,7 @@
       class="card max-lg:card-compact rounded-2xl shadow-xl w-full max-sm:aspect-[2/1] md:aspect-[2/1] lg:w-[26rem] lg:h-[12rem] mb-8"
       :class="[bgStyle, textStyle]"
     >
-      <div class="card-body">
+      <div class="card-body rounded-2xl hover:glass">
         <h2 class="card-title whitespace-nowrap">{{ title }}</h2>
         <p>{{ desc }}</p>
         <div class="card-actions justify-end">
