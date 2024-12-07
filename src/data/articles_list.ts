3a06e878bae02856b480b82a5e43865d890ef302
idealclover <idealclover@live.com>
2024-12-08 02:40:36 +0800

    👴 更新啦！

--- src/data/articles_list.ts
+++ src/data/articles_list.ts
@@ -4,6 +4,16 @@ export default [
     url: "https://idealclover.top/articles.html",
     articles: [
       {
+        title: "好评喵：轻松生成商品 / 服务评价",
+        url: "https://idealclover.top/archives/641",
+        time: "2024.11.17",
+      },
+      {
+        title: "2024 Q3 总结：灰烬里重新生根发芽",
+        url: "https://idealclover.top/archives/640",
+        time: "2024.11.09",
+      },
+      {
         title: "应急储备包制作经验分享：打造个人应急 “救生圈”",
         url: "https://idealclover.top/archives/639",
         time: "2024.09.30",
@@ -17,16 +27,6 @@ export default [
         title: "用AI为Typecho博客文章生成摘要吧！",
         url: "https://idealclover.top/archives/636",
         time: "2023.12.03",
-      },
-      {
-        title: "Caritas APP 2.0 版本发布",
-        url: "https://idealclover.top/archives/635",
-        time: "2022.08.21",
-      },
-      {
-        title: "友链屋 - 建个网站订阅友链们的更新～",
-        url: "https://idealclover.top/archives/634",
-        time: "2022.05.22",
       }
     ],
   },
@@ -34,6 +34,11 @@ export default [
     title: "技术",
     url: "https://idealclover.top/category/blog",
     articles: [
+      {
+        title: "好评喵：轻松生成商品 / 服务评价",
+        url: "https://idealclover.top/archives/641",
+        time: "2024.11.17",
+      },
       {
         title: "用AI为Typecho博客文章生成摘要吧！",
         url: "https://idealclover.top/archives/636",
@@ -53,18 +58,18 @@ export default [
         title: "为博客添加 COVID-19 疫情地图",
         url: "https://idealclover.top/archives/632",
         time: "2022.05.04",
-      },
-      {
-        title: "2020 年，或许你应当试试这样记笔记",
-        url: "https://idealclover.top/archives/625",
-        time: "2020.06.14",
-      },
+      }
     ],
   },
   {
     title: "生活",
     url: "https://idealclover.top/category/life",
     articles: [
+      {
+        title: "2024 Q3 总结：灰烬里重新生根发芽",
+        url: "https://idealclover.top/archives/640",
+        time: "2024.11.09",
+      },
       {
         title: "2021-2023年终总结：跃入人海，潮起潮落",
         url: "https://idealclover.top/archives/637",
@@ -84,12 +89,7 @@ export default [
         title: "Hello&bye, days",
         url: "https://idealclover.top/archives/626",
         time: "2020.10.01",
-      },
-      {
-        title: "南哪课表的钱捐哪了",
-        url: "https://idealclover.top/archives/609",
-        time: "2020.02.29",
-      },
+      }
     ],
   },
   {
